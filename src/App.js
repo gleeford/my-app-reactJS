d9bedf5de1efa01271f83323010b72134b7ccb5b
gleeford <mgmromasanta@gmail.com>
2023-02-22 10:38:13 +0800

    continuation of fourth lesson, added onclick functions and borders and buttons

--- src/App.js
+++ src/App.js
@@ -37,38 +37,58 @@ import logo from "./logo.svg";
 // }
 
 // ~~~~~ Fourth Lesson After Components ~~~~~~~~~~~~~~~
-function App() {
+function App({ title }) {
   return (
-    <div className="App">
-      <Product
-        img="https://cdn.shopify.com/s/files/1/1622/9929/products/Adidas_Yeezy_Boost_350_Low_Black_V2_Men_s_grande.png?v=1560321730"
-        name="Yeezy 700"
-        description="Black Shoes For Men"
-        price="PHP 10000"
-      />
+    <div>
+      <h1 style={heading}>{title}</h1>
+      <div className="App">
+        <Product
+          img="https://cdn.shopify.com/s/files/1/1622/9929/products/Adidas_Yeezy_Boost_350_Low_Black_V2_Men_s_grande.png?v=1560321730"
+          name="Yeezy 700"
+          description="Black Shoes For Men"
+          price="PHP 10000"
+        />
 
-      <Product
-        img="https://static.cloud-boxloja.com/lojas/ym522/produtos/a69892ae-3b38-460a-b60b-fb24cb5a2bb0.jpg"
-        name="Air Jordan 1"
-        description="Hype Shoes For Men"
-        price="PHP 30000"
-      />
+        <Product
+          img="https://static.cloud-boxloja.com/lojas/ym522/produtos/a69892ae-3b38-460a-b60b-fb24cb5a2bb0.jpg"
+          name="Air Jordan 1"
+          description="Hype Shoes For Men"
+          price="PHP 30000"
+        />
 
-      <Product
-        img="https://nb.scene7.com/is/image/NB/bb550sn1_nb_02_i?$pdpflexf2$&wid=440&hei=440"
-        name="New Balance 550"
-        description="Trendy Shoes For Men"
-        price="PHP 9000"
-      />
+        <Product
+          img="https://nb.scene7.com/is/image/NB/bb550sn1_nb_02_i?$pdpflexf2$&wid=440&hei=440"
+          name="New Balance 550"
+          description="Trendy Shoes For Men"
+          price="PHP 9000"
+        />
 
-      <Product
-        img={logo}
-        name="React logo"
-        description="Trendy Shoes For Men"
-        price="PHP 9000"
-      />
+        <Product
+          img={logo}
+          name="React logo"
+          description="Trendy Shoes For Men"
+          price="PHP 9000"
+        />
+
+        <Product
+          img={logo}
+          name="React logo"
+          description="Trendy Shoes For Men"
+          price="PHP 9000"
+        />
+      </div>
     </div>
   );
 }
+
+App.defaultProps = {
+  title: "Shoe Products",
+};
+
+const heading = {
+  color: "white",
+  background: "black",
+  textAlign: "center",
+};
 
 export default App;
--- src/component/Product.js
+++ src/component/Product.js
@@ -1,12 +1,24 @@
-function Product(props){
-    return(
-// image, name, description, price 
-        <div>
-            <img src={props.img} alt="product"/>
-            <h3>{props.name}</h3>
-            <p>{props.description}</p>
-            <h4>{props.price}</h4>
-        </div>
-    );
+function Product(props) {
+    const clickCart = () => {
+        alert("Add to Cart")
+    }
+    const clickFave = () => {
+        alert("Add to Favorites")
+    }
+  return (
+    // image, name, description, price
+    <div className="container">
+      <img src={props.img} alt="product" width="200px" height="200px" />
+      <h3>{props.name}</h3>
+      <p>{props.description}</p>
+      <h4>{props.price}</h4>
+      <button className="btn" style={{ background: "black" }} onClick={clickCart}>
+        Add to Cart
+      </button>
+      <button className="btn" style={{ background: "rgb(45, 151, 89)" }} onClick={clickFave}>
+        Add to Favorites
+      </button>
+    </div>
+  );
 }
 export default Product;
